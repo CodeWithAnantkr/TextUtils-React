75c57a982a7f754fc5f204c2e49e76fc0904fd4b
CodeWithAnantkr <anantkumar462@gmail.com>
2023-03-25 11:01:28 +0530

    Initial commit

---
+++ module1.mjs
@@ -0,0 +1,8 @@
+// import dxa, {a,c,d,e } from './module2.mjs'
+// console.log(dxa);
+console.log(a);
+console.log(c);
+console.log(d);
+console.log(e);
+
+import  {a,c,d,e } from './module2.mjs'
---
+++ module2.mjs
@@ -0,0 +1,11 @@
+const a = "Nida";
+const b = "Anant";
+const c = "Ashish";
+const d = "Naghma";
+const e = "Gudiya";
+
+export  default b;
+export {a};
+export {c};
+export {d};
+export {e};
